5ee5ef83d8732911d38c3b94de57ff23f45e0aa3
Ruben Cordero <ruben.cordero29@yahoo.com>
2021-12-01 21:10:10 -0600

    final

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 // import axios here
 
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
-// for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const axios = require('axios');
 
-const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+// Please note that it is normally not considered best practice to commit
+// api keys to github as it presents a security risk. It is done here only
+// for practice purposes as we are sharing the same account
+const api_key = 'd771b19ef336ed8381def3a60b574464';
+
+function discoverMovie() {
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
   // code here
+  return axios(url);
+  // .then (function(response){
+  //  return response.data.results
 }
 
 const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
-}
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`;
+  return axios(url).then(function (response) {
+    return response.data;
+  });
+};
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  const fakeId = 1; // FAKE ID HERE
+  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`;
   // code here
-}
-
-
+  return axios(url)
+    .then(function (response) {
+      return response.status;
+    })
+    .catch(function (error) {
+      return error.response.status;
+    });
+};
 
 module.exports = {
   discoverMovie,
   getMovieById,
-  getMovieByIdFailure
-}
+  getMovieByIdFailure,
+};
